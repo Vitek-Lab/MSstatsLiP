170901b12eae3600492ef1281ccf43ade99e7b1e
devonjkohler <devonjkohler@gmail.com>
2022-04-18 15:51:24 -0400

    new bioc version changes

--- R/ResistanceBarcodePlotLiP.R
+++ R/ResistanceBarcodePlotLiP.R
@@ -3,7 +3,7 @@
 #'
 #' @export
 #' @import ggplot2
-#' @importFrom data.table as.data.table `:=` rbindlist merge
+#' @importFrom data.table as.data.table `:=` rbindlist
 #' @importFrom stringr str_match str_split
 #' @importFrom grDevices dev.off hcl pdf
 #'
@@ -41,79 +41,25 @@ ResistanceBarcodePlotLiP = function(data,
                            fasta,
                            which.prot = "all",
                            which.condition = "all",
-                           differential_analysis = data.frame(),
+                           differential_analysis = FALSE,
                            which.comp = "all",
+                           adj.pvalue.cutoff = .05,
+                           FC.cutoff = 0,
                            width = 12,
                            height = 4,
                            address = ""){
-  # TODO: Figure out if exception should be calculated here or in tryptic function
-  # TODO: Add checks for resistance barcode
-  # .checkBarcodeParams(data, fasta, model_type, which.prot, which.comp,
-  #                     width, height, address)
-
-  fully_TRI = ProteinName = uniprot_iso = Label = NULL
-  PeptideSequence = sig = Coverage = Index = NULL
-
-  ## Calculate tryptic
-  calc_data = data$LiP$ProteinLevelData[, c("FULL_PEPTIDE", "Protein")]
-  calc_data$PeptideSequence = unlist(
-    lapply(calc_data$FULL_PEPTIDE, function(x){str_split(x, "_")[[1]][[2]]}))
-  setnames(calc_data, c("Protein"), c("ProteinName"))
-  tryptic_info = calculateTrypticity(calc_data, fasta)
-  tryptic_info$FULL_PEPTIDE = paste(tryptic_info$ProteinName,
-                                    tryptic_info$PeptideSequence, sep = "_")
-
-  # Extract correct data.frames from input data
-  lip_features = data$LiP$ProteinLevelData[, c("FULL_PEPTIDE", "Protein",
-                                               "LogIntensities", "GROUP", "SUBJECT")]
-  lip_features = as.data.table(lip_features)
-  lip_features = lip_features[, mean(LogIntensities),
-                              by = list(FULL_PEPTIDE, Protein, GROUP)]
-  trp_features = data$TrP$ProteinLevelData[, c("Protein", "LogIntensities",
-                                               "GROUP", "SUBJECT")]
-  trp_features = as.data.table(trp_features)
-  trp_features = trp_features[, mean(LogIntensities), by = list(Protein, GROUP)]
 
   # Load and format Fasta file
-  formated_fasta = tidyFasta(fasta)
-  formated_fasta = as.data.table(formated_fasta)
+  ## Make sure file is loaded into memory
+  if (identical(typeof(fasta_file), "character")){
+    fasta_file = tidyFasta(fasta_file)
+  }
+  formated_fasta = as.data.table(fasta_file)
   formated_fasta = formated_fasta[, c("sequence", "uniprot_iso")]
 
-  ## Test for missing proteins in FASTA file
-  model.proteins = unique(c(lip_features$PROTEIN,
-                            as.character(trp_features$PROTEIN)))
-  fasta.proteins = unique(formated_fasta[, uniprot_iso])
-
-  missing = setdiff(model.proteins, fasta.proteins)
-
-  if (!identical(missing, character(0))){
-    message(paste0("The following proteins are present in the data but ",
-                   "not in the FASTA file. Note proteins must be in the FASTA ",
-                   "in order to be plotted: ", paste(missing, collapse=", "))
-    )
-  }
-
-  # Calculate Resistance for each peptide
-  # Join lip and trp
-  joined_data = merge(lip_features, trp_features, by = c("Protein", "GROUP"),
-                      all.x = TRUE)
-
-  ## Bring tryptic info into data
-  joined_data = merge(joined_data, tryptic_info, all.x = TRUE,
-                      by = "FULL_PEPTIDE")
-  joined_data = joined_data[fully_TRI == TRUE, ]
-
-  joined_data$FULL_PEPTIDE = unlist(
-    lapply(joined_data$FULL_PEPTIDE, function(x){str_split(x, "_")[[1]][[2]]}))
-  joined_data$Accessibility_ratio = 2 ^ (joined_data$V1.x - joined_data$V1.y)
-  joined_data$Accessibility_ratio = ifelse(joined_data$Accessibility_ratio > 1,
-                                           1., joined_data$Accessibility_ratio)
-
   ## Bring sequence into data
-  coverage.df = merge(joined_data, formated_fasta, all.x = TRUE,
-                       by.x = "Protein", by.y = "uniprot_iso")
-
-
+  coverage.df = merge(data$RunLevelData, formated_fasta, all.x = TRUE,
+                      by.x = "Protein", by.y = "uniprot_iso")
 
   ## Create PDF to save plots if requested
   if (address != FALSE) {
@@ -132,12 +78,12 @@ ResistanceBarcodePlotLiP = function(data,
 
   ## Determine which proteins to plot
   if (which.condition == "all"){
-    which.cond = unique(coverage.df[, GROUP])
+    which.condition = unique(coverage.df[, GROUP])
   }
 
-  for (c in seq(length(which.cond))){
+  for (c in seq(length(which.condition))){
 
-    cond.coverage.df = coverage.df[GROUP == which.cond[[c]], ]
+    cond.coverage.df = coverage.df[GROUP == which.condition[[c]], ]
 
     if (which.prot == "all"){
       which.prot = unique(cond.coverage.df[, Protein ])
@@ -153,7 +99,7 @@ ResistanceBarcodePlotLiP = function(data,
       temp.coverage.df = cond.coverage.df[Protein == which.prot[[i]], ]
 
       for (idx in seq(nrow(temp.coverage.df))){
-        cov_idx = gregexpr(pattern = temp.coverage.df[idx, FULL_PEPTIDE],
+        cov_idx = gregexpr(pattern = temp.coverage.df[idx, PeptideSequence],
                             temp.seq)
         start = cov_idx[[1]][1] - 1
         end = start + attr(cov_idx[[1]],'match.length')
@@ -168,7 +114,7 @@ ResistanceBarcodePlotLiP = function(data,
         geom_col(aes(x = Index, y = 10, fill = Accessibility_ratio), width = 1) +
         scale_fill_gradient(low = "yellow", high = "red", limits = c(0,1),
                             name = "Proteolytic Resistance") +
-        labs(title = paste0(which.prot[[i]], " Coverage - ", which.cond[[c]]),
+        labs(title = paste0(which.prot[[i]], " Coverage - ", which.condition[[c]]),
              x = "Amino Acid Sequence", y = "") +
         theme(axis.text.y = element_blank(),
               axis.ticks.y = element_blank(),
@@ -177,55 +123,69 @@ ResistanceBarcodePlotLiP = function(data,
     }
   }
 
-  # if (nrow(plot_differential) > 0){
-  #   model_data = differential_analysis$Adjusted.LiP.Model
-  #   if (which.comp == "all"){
-  #     which.comp = unique(model_data[, Label])
-  #   }
   #
-  #   for (c in seq(length(which.comp))){
-  #
-  #     cond.coverage.df = coverage.df[GROUP == which.cond[[c]], ]
-  #
-  #     if (which.prot == "all"){
-  #       which.prot = unique(cond.coverage.df[, Protein ])
-  #     }
-  #
-  #     for (i in seq(length(which.prot))){
-  #
-  #       ## Build coverage data.table
-  #       temp.seq = formated_fasta[uniprot_iso == which.prot[[i]], sequence]
-  #       coverage.index = data.table("Index" = seq_len(nchar(temp.seq)),
-  #                                   "Accessibility_ratio" = 0)
-  #
-  #       temp.coverage.df = cond.coverage.df[Protein == which.prot[[i]], ]
-  #
-  #       for (idx in seq(nrow(temp.coverage.df))){
-  #         cov_idx = gregexpr(pattern = temp.coverage.df[idx, FULL_PEPTIDE],
-  #                            temp.seq)
-  #         start = cov_idx[[1]][1] - 1
-  #         end = start + attr(cov_idx[[1]],'match.length')
-  #
-  #         for (j in start:end){
-  #           coverage.index[j, Accessibility_ratio := temp.coverage.df[idx, Accessibility_ratio]]
-  #         }
-  #       }
-  #       coverage.index$Accessibility_ratio = ifelse(coverage.index$Accessibility_ratio == 0.,
-  #                                                   NA, coverage.index$Accessibility_ratio)
-  #       barcode_plot = ggplot(data = coverage.index) +
-  #         geom_col(aes(x = Index, y = 10, fill = Accessibility_ratio), width = 1) +
-  #         scale_fill_gradient(low = "yellow", high = "red", limits = c(0,1),
-  #                             name = "Proteolytic Resistance") +
-  #         labs(title = paste0(which.prot[[i]], " Coverage - ", which.cond[[c]]),
-  #              x = "Amino Acid Sequence", y = "") +
-  #         theme(axis.text.y = element_blank(),
-  #               axis.ticks.y = element_blank(),
-  #               panel.background = element_rect(fill = 'white', colour = 'white'))
-  #       print(barcode_plot)
-  #     }
-  #   }
-  #
-  # }
+  if (differential_analysis  == TRUE){
+    model.data = data$groupComparison
+    model.data$sig = (model.data$adj.pvalue < adj.pvalue.cutoff &
+                          abs(model.data$log2FC) >= FC.cutoff)
+
+    coverage.df = model.data[, c("Protein", "PeptideSequence",
+                                  "Label", "sig")]
+    ## Bring sequence into data
+    coverage.df = merge(coverage.df, formated_fasta, all.x = TRUE,
+                        by.x = "Protein", by.y = "uniprot_iso")
+    if (which.comp == "all"){
+      which.comp = unique(coverage.df[, Label])
+    }
+
+    for (c in seq(length(which.comp))){
+
+      cond.coverage.df = coverage.df[Label == which.comp[[c]], ]
+
+      if (which.prot == "all"){
+        which.prot = unique(cond.coverage.df[, Protein ])
+      }
+
+      for (i in seq(length(which.prot))){
+
+        ## Build coverage data.table
+        temp.seq = formated_fasta[uniprot_iso == which.prot[[i]], sequence]
+        coverage.index = data.table("Index" = seq_len(nchar(temp.seq)),
+                                    "Coverage" = "No Coverage")
+
+        temp.coverage.df = cond.coverage.df[Protein == which.prot[[i]], ]
+
+        for (idx in seq(nrow(temp.coverage.df))){
+          cov_idx = gregexpr(pattern = temp.coverage.df[idx, PeptideSequence],
+                             temp.seq)
+          start = cov_idx[[1]][1] - 1
+          end = start + attr(cov_idx[[1]],'match.length')
+
+          for (j in start:end){
+            if (temp.coverage.df[idx, sig] == TRUE){
+              coverage.index[j, Coverage := 'Significant']
+            } else if (temp.coverage.df[idx, sig] == FALSE &
+                       coverage.index[j, Coverage] != 'Significant'){
+              coverage.index[j, Coverage := 'Insignificant']
+            }
+          }
+        }
+
+        barcode_plot <- ggplot(data = coverage.index) +
+          geom_col(aes(x = Index, y = 10, fill = Coverage), width = 1) +
+          scale_fill_manual(values = c('Significant' = '#FF0000',
+                                       'Insignificant' = '#808080',
+                                       'No Coverage' = '#000000')) +
+          labs(title = paste0(which.prot[[i]], " Coverage - ", which.comp[[c]]),
+               x = "Amino Acid Sequence", y = "") +
+          theme(axis.text.y = element_blank(),
+                axis.ticks.y = element_blank(),
+                panel.background = element_rect(fill = 'white', colour = 'white'))
+        print(barcode_plot)
+      }
+    }
+
+  }
 
   if (address != FALSE) {
     dev.off()
--- R/calculateTrypticity.R
+++ R/calculateTrypticity.R
@@ -15,43 +15,54 @@
 #' @examples
 #' fasta <- tidyFasta(system.file("extdata", "ExampleFastaFile.fasta", package="MSstatsLiP"))
 #' calculateTrypticity(MSstatsLiP_data$LiP, fasta)
-calculateTrypticity <- function(LiP_data, fasta_file){
+calculateTrypticity = function(LiP_data, fasta_file){
 
-  unique_pep <- unique(LiP_data[, c("PeptideSequence",
+  unique_pep = unique(LiP_data[, c("PeptideSequence",
                                     "ProteinName")])
 
+  ## Make sure file is loaded into memory
   if (identical(typeof(fasta_file), "character")){
-    fasta_file <- tidyFasta(fasta_file)
+    fasta_file = tidyFasta(fasta_file)
   }
 
   ## Add fasta file to get full sequence
-  combined_df <- merge(unique_pep, fasta_file, all.x = TRUE,
+  combined_df = merge(unique_pep, fasta_file, all.x = TRUE,
                        by.x = "ProteinName", by.y = "uniprot_iso")
 
     ## Identify pre/end/post AA
-  combined_df$start <- mapply(function(x,y){gregexpr(x, y)[[1]][1] - 1},
+  combined_df$start = mapply(function(x,y){gregexpr(x, y)[[1]][1] - 1},
                               combined_df$PeptideSequence, combined_df$sequence)
-  combined_df$end <- combined_df$start + nchar(combined_df$PeptideSequence)
-  combined_df$preAA <- mapply(function(x,y) {substr(y, x, x)},
+  combined_df$end = combined_df$start + nchar(combined_df$PeptideSequence)
+  combined_df$preAA = mapply(function(x,y) {substr(y, x, x)},
                               combined_df$start, combined_df$sequence)
-  combined_df$endAA <- mapply(function(x,y) {substr(y, x, x)},
+  combined_df$endAA = mapply(function(x,y) {substr(y, x, x)},
                               combined_df$end, combined_df$sequence)
-  combined_df$postAA <- mapply(function(x,y) {substr(y, x, x)},
+  combined_df$postAA = mapply(function(x,y) {substr(y, x, x)},
                                combined_df$end + 1, combined_df$sequence)
 
   ## Determine trip
-  combined_df$fully_TRI <- (combined_df$preAA %in% c("K", "R") &
-                                    combined_df$endAA %in% c("K", "R"))
-  combined_df$NSEMI_TRI <- (combined_df$preAA %in% c("K", "R") &
-                                    !combined_df$endAA %in% c("K", "R"))
-  combined_df$CSEMI_TRI <- (!combined_df$preAA %in% c("K", "R") &
-                                    combined_df$endAA %in% c("K", "R"))
-  combined_df$CTERMINUS <- combined_df$postAA == ""
-  combined_df$NTERMINUS <- combined_df$start == 1
-  combined_df$StartPos <- combined_df$start
-  combined_df$EndPos <- combined_df$end
+  combined_df$fully_TRI = (combined_df$preAA %in% c("K", "R") &
+                             combined_df$endAA %in% c("K", "R") &
+                             combined_df$postAA != "P")
+  combined_df$NSEMI_TRI = (combined_df$preAA %in% c("K", "R") &
+                             !combined_df$endAA %in% c("K", "R") &
+                             combined_df$postAA != "P")
+  combined_df$CSEMI_TRI = (!combined_df$preAA %in% c("K", "R") &
+                             combined_df$endAA %in% c("K", "R") &
+                             combined_df$postAA != "P")
+  combined_df$CTERMINUS = combined_df$postAA == ""
+  combined_df$NTERMINUS = combined_df$start == 1
+  combined_df$MISSED = ifelse((str_count(combined_df$PeptideSequence, "K") -
+                                 str_count(combined_df$PeptideSequence, "KP") +
+                                 str_count(combined_df$PeptideSequence, "R") -
+                                 str_count(combined_df$PeptideSequence, "RP")
+                               ) > 1, TRUE, FALSE)
+  combined_df$StartPos = combined_df$start
+  combined_df$EndPos = combined_df$end
+
+
 
   return(combined_df[, c("ProteinName", "PeptideSequence", "fully_TRI",
                          "NSEMI_TRI", "CSEMI_TRI", "CTERMINUS", "NTERMINUS",
-                         "StartPos", "EndPos")])
+                         "MISSED", "StartPos", "EndPos")])
 }
